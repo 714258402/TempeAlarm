867f5c1e78d9708b9b9641588e15293a9b8cf839
崧玮 李 <714258402@qq.com>
2020-08-06 08:19:20 +0800

    1.0.0.7

---
+++ TempeAlarm/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace TempeAlarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Tempe Alarm";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(204, 204);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 21);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            87,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 213);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Alarm Tempe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Max Tempe：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(161, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(346, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "1.0.0.3";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2,
+            this.toolStripMenuItem3});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(180, 70);
+            this.contextMenuStrip1.Text = "Options";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "Show Window";
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem2.Text = "Min Window";
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem3.Text = "Close TempAlarm";
+            this.toolStripMenuItem3.Click += new System.EventHandler(this.toolStripMenuItem3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 259);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tempe Alarm";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem3;
+    }
+}
+
---
+++ TempeAlarm/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Timers;
+using OpenHardwareMonitor.Hardware;
+using OpenHardwareMonitor;
+using System.Runtime.InteropServices;
+
+namespace TempeAlarm
+{
+
+    public partial class Form1 : Form
+    {
+        static Double CPUTempe = 0;
+        static string TopCore = "Core?";
+        UpdateVisitor updateVisitor = new UpdateVisitor();
+        Computer computer = new Computer();
+        int i = 0, j = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                //notifyIcon1.Visible = true; //托盘图标
+            }
+            if (this.WindowState == FormWindowState.Minimized)//最小化事件
+            {
+                //notifyIcon1.Visible = true;//托盘图标
+                Invoke((MethodInvoker)delegate {
+                    this.Hide();//最小化时窗体隐藏
+                });
+                }
+        }
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Invoke((MethodInvoker)delegate {
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    this.Show();
+                    this.WindowState = FormWindowState.Normal; //还原窗体
+                }
+            });
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Enabled = true;
+            timer.Interval = 10000; //执行间隔时间,单位为毫秒; 这里实际间隔为10s  
+            timer.Start();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(CheckTempe);
+            System.Timers.Timer tm = new System.Timers.Timer(1000);
+            tm.AutoReset = false;
+            tm.Enabled = true;
+            tm.Elapsed += new ElapsedEventHandler(Miniwindow);
+        }
+        private void Miniwindow(object source, ElapsedEventArgs e)
+        {
+            Invoke((MethodInvoker)delegate {this.WindowState = FormWindowState.Minimized; });
+            ((System.Timers.Timer)source).Enabled = false;
+        }
+
+        private void CheckTempe(object source, ElapsedEventArgs e)
+        {
+            CPUTempe = 0;
+
+            computer.Open();
+            computer.Accept(updateVisitor);
+
+            for (i = 0; i < computer.Hardware.Length; i++)
+            {
+                //循环找到HardwareType为cpu
+                if ((computer.Hardware[i].HardwareType == HardwareType.CPU) || (computer.Hardware[i].HardwareType == HardwareType.GpuNvidia) || (computer.Hardware[i].HardwareType == HardwareType.GpuAti))   //(computer.Hardware[i].HardwareType == HardwareType.CPU) ||
+                {
+                    for (j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                    {
+
+                        //找到温度
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+                        {
+                            if (Convert.ToDouble(computer.Hardware[i].Sensors[j].Value.ToString()) > CPUTempe)
+                            {
+                                CPUTempe = Convert.ToDouble(computer.Hardware[i].Sensors[j].Value.ToString());
+                                TopCore = computer.Hardware[i].Sensors[j].Name;
+                            }
+                        }
+                    }
+                }
+            }
+            Invoke(
+                (MethodInvoker)delegate
+                { label3.Text = TopCore + "温度：" + CPUTempe.ToString() + "℃"; });
+            if (CPUTempe > Convert.ToDouble(numericUpDown1.Value))
+            {
+                Console.Beep(350, 2000);
+                MessageBoxTimeOut mb = new MessageBoxTimeOut();
+                mb.Show(9000, TopCore + "温度：" + CPUTempe.ToString() + "℃", "警告(窗体9秒后自动关闭...)");
+            }
+            
+        }
+
+
+        private class UpdateVisitor : IVisitor
+        {
+            public void VisitComputer(IComputer computer)
+            {
+                computer.Traverse(this);
+            }
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware)
+                    subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Invoke((MethodInvoker)delegate {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;//显示窗口
+            });
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Invoke((MethodInvoker)delegate {
+                this.WindowState = FormWindowState.Minimized;//最小化
+                this.Hide();
+            });
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();//退出程序
+        }
+
+        private class MessageBoxTimeOut
+        {
+            private string _caption;
+
+            public void Show(string text, string caption)
+            {
+                Show(3000, text, caption);
+            }
+            public void Show(int timeout, string text, string caption)
+            {
+                Show(timeout, text, caption, MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            public void Show(int timeout, string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon)
+            {
+                this._caption = caption;
+                StartTimer(timeout);
+                MessageBox.Show(text, caption, buttons, icon);
+            }
+            private void StartTimer(int interval)
+            {
+                System.Timers.Timer ktimer = new System.Timers.Timer();
+                ktimer.Interval = interval;
+                ktimer.Elapsed += new System.Timers.ElapsedEventHandler(Timer_Tick);
+                ktimer.Enabled = true;
+            }
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                KillMessageBox();
+                //停止计时器
+                ((System.Timers.Timer)sender).Enabled = false;
+            }
+            [DllImport("User32.dll", EntryPoint = "FindWindow", CharSet = CharSet.Auto)]
+            private extern static IntPtr FindWindow(string lpClassName, string lpWindowName);
+            [DllImport("User32.dll", CharSet = CharSet.Auto)]
+            public static extern int PostMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+            public const int WM_CLOSE = 0x10;
+            private void KillMessageBox()
+            {
+                //查找MessageBox的弹出窗口,注意对应标题
+                IntPtr ptr = FindWindow(null, this._caption);
+                if (ptr != IntPtr.Zero)
+                {
+                    //查找到窗口则关闭
+                    PostMessage(ptr, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+                }
+            }
+        }
+
+
+    }
+
+
+
+}
